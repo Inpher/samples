41550b4ae45b0ec419445296d66744aca67c8d35
jetchev <dimitar.jetchev@epfl.ch>
2015-10-27 16:38:58 +0100

    modified script archive.sh GUI-27

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,14 +1,24 @@
-rm -rf samples
-git archive -o samples.zip HEAD
-mkdir samples
-unzip samples.zip -d samples/
-rm samples/.gitignore
-rm samples.zip
-cd samples
-mkdir lib
-mkdir bin
-cp ../scripts/*.sh bin/
-cp ../scripts/*.command bin/
-cp ../lib/* lib/
+cd ../inpher-ux/
+mvn package
 cd ..
-zip -r9 samples.zip samples/
+rm -rf inpher-sdk
+mkdir inpher-sdk 
+cd inpher-sdk
+mkdir lib 
+mkdir bin
+cp ../inpher-ux/target/lib/* lib/
+cp ../inpher-ux/target/inpher-ux-0.6.jar lib/
+cp ../scripts/setup.sh bin/setup.sh
+cp ../scripts/setup.command bin/mac_setup.command
+cp ../scripts/setup.cmd bin/win_setup.cmd
+cp ../scripts/gui.sh bin/gui.sh
+cp ../scripts/gui.command bin/mac_gui.command
+cp ../scripts/gui.cmd bin/win_gui.cmd
+mkdir inpher-samples
+cp ../inpher-samples/.classpath inpher-samples/
+cp ../inpher-samples/.project inpher-samples/
+cp -Rp ../inpher-samples/.settings inpher-samples/
+cp ../inpher-samples/pom.xml inpher-samples/
+cp -Rp ../inpher-samples/src inpher-samples/
+cd ..
+zip -r9 inpher-sdk.zip inpher-sdk/
